051b1ae52350fe681f47415a969e39b4b632358c
CyTon Program Languages <cytoncode@gmail.com>
2021-01-30 21:11:21 +0200

    Update TerModula.py

--- TerModula.py
+++ TerModula.py
@@ -1,1 +1,20 @@
+def Module(AddressModule, ARGS):
+  pass  # 
 
+
+def Terminal(Cmd, KeyList, FlagList, List):
+  pass  # Модуль принимает от этой функции (KeyList, FlagList, List).
+  pass  # Встроенный функционал просто 
+  pass  # 
+  pass  # 
+
+def TermFileToPsevdo_Arg():
+  pass  # Функция для обработки файла(cmd/bat) в приличные аргументы терминала.
+
+
+def TermArgToPsevdo_Arg():
+  pass  # Функция для обработки аргументов приложения(TerModula -arg (echo "Hello, World!\n")) в приличные аргументы терминала.
+
+
+def TermListToPsevdo_Arg():
+  pass  # Функция для обработки вводимого текста с консоли в приличные аргументы терминала.
